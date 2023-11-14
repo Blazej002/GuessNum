3215cc3c08f04b5e809460c0e48ce3bbfc2e9880
Blazej002 <141903573+Blazej002@users.noreply.github.com>
2023-11-14 12:48:50 +0100

    Add project files.

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace GuessNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool win = false;
+            int ranNumber = genNum();
+            Console.WriteLine("Hi!! Welcome!");
+            Console.WriteLine("Guess the number");
+            Console.WriteLine(ranNumber);
+            while (!win)
+            {
+                Console.WriteLine("_________");
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+                
+                win = checkIf(ranNumber, userGuess);
+                
+                if (win == false)
+                {
+                    Console.WriteLine("Wrong");
+                    continue;
+                }
+
+                Console.WriteLine("_______________________");
+                Console.WriteLine("_______________________");
+                Console.WriteLine("You're correct, congrats.");
+                Console.WriteLine($"The number was {ranNumber}");
+            }
+        }
+
+        static public int genNum()
+        {
+            Random ran = new Random();
+
+            int ranNum = ran.Next(1, 100);
+            return ranNum;
+        }
+
+        static public bool checkIf(int x, int y)
+        {
+            if (x == y)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
